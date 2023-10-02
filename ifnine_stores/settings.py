4c8a740ed8f7cd4e03dab826f6b93f348101cf38
Onyekachiii <stiloonwa4u@yahoo.com>
2023-10-02 08:44:47 +0400

    Build: added versions to requirement.txt

--- ifnine_stores/settings.py
+++ ifnine_stores/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6i#18@0-671m0w89im!meiwi9m=vnmni_me2m4rv4enon30im@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', "False").lower == "true"
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split('')
 
 
 # Application definition
