a43668f6cbb696c34c380847f7f0ef98754bf3e2
Onyekachiii <stiloonwa4u@yahoo.com>
2023-10-02 10:48:40 +0400

    Added staticfiles_storage

--- ifnine_stores/settings.py
+++ ifnine_stores/settings.py
@@ -147,6 +147,8 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
