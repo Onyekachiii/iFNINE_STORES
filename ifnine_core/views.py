ae9a44f7065f52ede1372143c3d67a654ce8a07f
Onyekachiii <stiloonwa4u@yahoo.com>
2023-10-03 10:20:21 +0400

    Changed favicon

--- ifnine_core/views.py
+++ ifnine_core/views.py
@@ -173,8 +173,10 @@ def search_view(request):
             "products": products,
         }
         return render(request, 'core/search.html', context)
+    
     else:
         messages.warning(request, "Please enter a search query")
+        return redirect('core:index')
 
 
 # To filter products by categories & vendors
